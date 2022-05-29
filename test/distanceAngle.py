eb5562f3fb806a5ba8bcc86cdd52dcf0515eee24
ChandanaMithra <chandanasubrahmanya@gmail.com>
2022-05-29 19:15:39 +0530

    returns distance and angle

---
+++ test/distanceAngle.py
@@ -0,0 +1,12 @@
+import math
+from math import atan2
+def getDistanceAngle(sourceX1,sourceY1,destX2,destY2):
+    underRoot= ((destX2-sourceX1)**2) + ((destY2-sourceY1)**2)
+    distInMeters= math.sqrt(underRoot)
+    dist= int(39.37*distInMeters)
+    Ax,Ay= destX2-sourceX1, destY2-sourceY1
+    angleInRad= math.atan2(Ay,Ax)
+    angleInDeg= int(57.2958*angleInRad)
+    print("distance in inches=",dist)
+    print("angle in degrees=", angleInDeg)
+getDistanceAngle(1,1,2,2)
